6e6e9bdc119f6fc7428fa5d9745d81bb5d332264
Fernanda Kipper Bucoski de Sousa <nanda.kipper@gmail.com>
2021-12-07 12:05:17 -0300

    feat: initiate react app manually

---
+++ Container/webpack.config.js
@@ -0,0 +1,42 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path')
+
+const isDev = process.NODE_ENV  !== 'production'
+
+module.exports = {
+  mode: isDev ? 'development' : 'production',
+  entry: path.resolve(__dirname, 'src', 'index.jsx'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      },
+    ]
+  },
+  devServer: {
+    static: path.resolve(__dirname, 'public'),
+    port: 3000,
+    historyApiFallback: true,
+    hot: false,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+      "Access-Control-Allow-Headers":
+        "X-Requested-With, content-type, Authorization",
+    },
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public', 'index.html'),
+    }),
+  ]
+}
